5fad12df5048484904542310638272304f0c4b86
fuad1706 <fuadshorlarxwix@gmail.com>
2024-01-13 11:46:22 -0800

    365

--- index.js
+++ index.js
@@ -12,3 +12,13 @@ hamburger.onclick = function(){
         hamburger.src = "./images/icon-hamburger.svg";
     }
 }
+
+
+// document.addEventListener('DOMContentLoaded', function () {
+    // const hamburger = document.querySelector('.hamburger');
+    // const nav = document.querySelector('nav');
+//   
+    // hamburger.addEventListener('click', function () {
+    //   nav.classList.toggle('open-nav');
+    // });
+//   });
